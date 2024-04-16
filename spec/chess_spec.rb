8b65cd1898730c22802e54b65f6c87aeeb85e4f3
Himumi <erliansyaha@gmail.com>
2024-04-16 21:36:14 +0900

    test: #valid_input? test

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,7 +1,9 @@
 require './lib/chess'
 
 describe Chess do
-  subject(:game) { described_class.new() }
+  let(:first) { double('Player' ) }
+  let(:last) { double('Player') }
+  subject(:game) { described_class.new(first, last) }
   describe '#create_board' do
     context 'when method is called' do
 
@@ -15,6 +17,22 @@ describe Chess do
       it 'last key is h0' do
         last_keys = board.keys.last
         expect(last_keys).to eq('h1')
+      end
+    end
+  end
+
+  describe '#valid_input?' do
+    context 'when user input value' do
+      it 'returns true if input is between a1..h8' do
+        input = "a1"
+        check_input = game.valid_input?(input)
+        expect(check_input).to eq(true)
+      end
+
+      it 'returns false if input is not between a1..h8' do
+        input = "l1"
+        check_input = game.valid_input?(input)
+        expect(check_input).not_to eq(true)
       end
     end
   end
