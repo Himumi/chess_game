315892b1931a120178d49b96ab02a378e770166d
Himumi <erliansyaha@gmail.com>
2024-04-16 21:50:03 +0900

    test: #convert_to_key test

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -55,4 +55,23 @@ describe Chess do
       end
     end
   end
+
+  describe '#convert_to_key' do
+    context 'when input is 00 (valid input)' do
+      it 'returns a1' do
+        input = "11"
+        converted_input = game.convert_to_key(input)
+        expect(converted_input).to eq("a1")
+      end
+    end
+
+    context 'when input is -10 (invalid input)' do
+      it 'returns nil' do
+        input = "-10"
+        converted_input = game.convert_to_key(input)
+        expect(converted_input).to be_nil
+      end
+
+    end
+  end
 end
