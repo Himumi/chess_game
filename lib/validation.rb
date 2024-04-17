781fb61f83b56aa741783f1fab4468229339dc3c
Himumi <erliansyaha@gmail.com>
2024-04-18 06:38:22 +0900

    fix: add "g"=>"7" and 7=>"g" to method

--- lib/validation.rb
+++ lib/validation.rb
@@ -9,7 +9,7 @@ module Validation
     return nil unless valid_input?(input)
     input = input.chars
 
-    letters = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "h" => 8}
+    letters = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8}
     input[0] = letters[input[0]]
     input.join
   end
@@ -18,7 +18,7 @@ module Validation
     input = input.chars
     return nil if invalid?(input)
 
-    letters = {"1" => "a", "2" => "b", "3" => "c", "4" => "d", "5" => "e", "6" => "f", "8" => "h" }
+    letters = {"1" => "a", "2" => "b", "3" => "c", "4" => "d", "5" => "e", "6" => "f", "7" => "g", "8" => "h" }
     input[0] = letters[input[0]]
     input.join
   end
