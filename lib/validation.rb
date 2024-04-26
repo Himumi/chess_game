d65ddc4a08853a0879a643813fcff5b7bcba7264
Himumi <erliansyaha@gmail.com>
2024-04-27 03:20:35 +0900

    refactor: create #en_passant? method in Validation module
      extract #en_passant? conditions into few methods, to improve
    readability.

--- lib/validation.rb
+++ lib/validation.rb
@@ -33,19 +33,47 @@ module Validation
     array.length > 3 or array.any? { |item| !item.between?("1", "8") }
   end
 
-  # def en_passant?(source, destination)
-  #   s_piece = game.board[source]
-  #   d_piece = game.board[destination]
+  def en_passant?(source, destination)
+    @en_passant = fulfilled_en_passant?(source, destination)
+  end
 
-  #   return false if !s_piece.role.eql?("pawn") or !d_piece.nil?
+  def fulfilled_en_passant?(source, destination)
+    s, d = source, destination
 
-  #   valid_round = (game.round - d_piece.round).eql?(1)
+    ep_input?(s, d) and ep_target?(s, d) and ep_need?(s, d) and ep_adjacent?(s, d)
+  end
 
-  #   s_number = convert_to_number(source).chars
-  #   d_number = convert_to_number(destination).chars
+  def ep_input?(source, destination)
+    @board[source].role.eql?("pawn") and @board[destination].nil?
+  end
 
-  #   neighbor = [-1, 1].include?(s_number[0].to_i - d_number[0].to_i)
+  def ep_target?(source, destination)
+    source_piece = @board[source]
+    target_piece = @board[source_piece.en_passant_piece(destination)]
 
-  #   d_piece.two_step and valid_round and neighbor
-  # end
+    target_piece.role.eql?("pawn") and !source_piece.color.eql?(target_piece.color)
+  end
+
+  def ep_need?(source, destination)
+    source_piece = @board[source]
+    target_piece = @board[source_piece.en_passant_piece(destination)]
+
+    target_piece.two_step and (@round - target_piece.round).eql?(1)
+  end
+
+  def ep_adjacent?(source, destination)
+    source_piece = @board[source]
+    destination = source_piece.en_passant_piece(destination)
+
+    s_key = convert_to_number(source).chars
+    a, b = s_key[0].to_i, s_key[1].to_i
+
+    adjacents = [
+      [a-1, b], [a+1, b]
+    ]
+
+    adjacents = adjacents.map { |v| convert_to_key(v.join) }
+
+    adjacents.include?(destination)
+  end
 end
